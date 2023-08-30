7785375166544b92c783cf58ca3a35bc1e56e331
jahir-raihan <raihan.joy10@gmail.com>
2023-08-30 18:05:01 +0600

    Updated & Fixed download feature

--- index.js
+++ index.js
@@ -120,11 +120,11 @@ class Calendar {
                 
                     if (this.verfiy_weekend(week_counter, this.time_zone)){
 
-                        week_template += `<li class="weekend-current-date"> ${day}</li>`;
+                        week_template += `<li class="weekend-current-date" id='active'> ${day}</li>`;
 
                     } else {
 
-                        week_template += `<li class="active"> ${day}</li>`;
+                        week_template += `<li class="active" id='active'> ${day}</li>`;
 
                     }
                     
@@ -183,150 +183,18 @@ calendar_init()
 
 // download the calendar as a PDF using jsPDF library
 
-function downloadAsPDF() {
-    window.jsPDF = window.jspdf.jsPDF;
+function download_calendar() {
+    
+    document.getElementById('header').style.display = 'none'
+    document.getElementById('search-selected-year-download').style.display = 'none'
+    document.getElementById('calender-header').style.marginTop = '2em'
+    let current_year = document.getElementsByClassName('calender-header-year')[0].innerHTML
+    document.getElementsByClassName('calender-header-year')[0].innerHTML = `Calendar - ${current_year}`
+    document.getElementById('active').classList.remove('active')
+    
+    window.print();
 
-    let calendar = document.getElementById("calender-container");
-    let calendar_months = document.querySelectorAll(".year-month-name");
-
-    // remove the active background
-    let active_month = document.querySelector(".active");
-    active_month.style.background = "none";
-
-    for (const calendar_month of calendar_months) {
-        calendar_month.style.color = "#343a40";
-        calendar_month.style.fontSize = "20px";
-        calendar_month.style.textAlign = "left";
-    }
-
-    // first convert the page to an image
-    html2canvas(calendar).then((canvas) => {
-        
-    // convert this canvas to a PDF
-    const dataURL = canvas.toDataURL('image/jpeg');
-
-    let doc = new jsPDF({orientation: 'p'});
-        doc.addImage(dataURL, 'JPEG', 12, 20, 180, 180);
-        doc.save('calendar.pdf');
-    });
-
-    //clear the style for the year-month-name class
-    calendar_months.forEach((cm) => {
-        cm.style.color = "white";
-        cm.style.fontSize = "13px";
-        cm.style.textAlign = "left";
-    })
-
-    // return the background color for the active class
-    active_month.style.background = "#343a40";
-
-
+    location.reload()
+    
 
 }
-
-// Function Based Approach
-
-// let current_date = new Date();
-// let months_list = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
-// function render_calendar(year){
-
-//     let calendar = ''
-
-//     // Iterating for 12 months
-//     for(var month = 0; month < 12; month++){
-      
-//         let current_itr_month = new Date(year, month, 1).getMonth();
-//         var month_template = `
-//         <div class="month">
-//             <p class="year-month-name">${months_list[month]} - ${year}</p>
-
-//             <div class="week-day-names">
-//                 <ul>
-//                     <li>Mo</li>
-//                     <li>Tu</li>
-//                     <li>We</li>
-//                     <li>Th</li>
-//                     <li>Fr</li>
-//                     <li>Sa</li>
-//                     <li>Su</li>
-//                 </ul>
-//             </div>
-
-//             <div class="days">
-              
-//         `
-//         let week_counter = 0;
-//         let first_week_itr = true;
-//         let week_template = '<div class="week-days"><ul>';
-//         for(var day = 1; day < 32; day++){
-
-//             let current_loop_date = new Date(year, month, day);
-
-//             // If days goes out of range, break out of the loop and start next month
-//             if (current_itr_month !== current_loop_date.getMonth()){
-//                 break
-//             } else {
-              
-//                 if(first_week_itr && current_loop_date.getDay() > 1){
-                  
-//                     for (var tmp = 0; tmp < current_loop_date.getDay() - 1; tmp++){
-//                         week_template += `<li></li>`;
-//                         week_counter += 1;
-                      
-//                     }
-                 
-                  
-//                 } else if (first_week_itr && current_loop_date.getDay() === 0){
-              
-//                     for (var tmp = 0; tmp < 6; tmp++){
-//                         week_template += `<li></li>`
-//                         week_counter += 1
-//                     }
-
-//                 } else {
-                  
-//                     if (week_counter % 7 === 0){
-//                         week_template += '</ul></div>'
-//                         month_template += week_template
-//                         week_template = '<div class="week-days"><ul>'
-//                         week_counter = 0
-
-//                     }
-//                 }
-//                 if (current_loop_date.getDate() == current_date.getDate() &&
-//                     current_loop_date.getMonth() === current_date.getMonth() &&
-//                     current_date.getFullYear() == current_loop_date.getFullYear()) {
-
-//                     week_template += `<li class="active"> ${day}</li>`
-
-//                 } else {
-//                     week_template += `<li>${day}</li>`
-//                 }
-              
-//                 week_counter += 1
-              
-//             }
-//             first_week_itr = false
-
-//         }
-//         week_template += '</ul></div>'
-//         month_template += week_template + '</div></div>'
-//         calendar += month_template
-      
-//     }
-//     document.getElementById('calender-container').innerHTML = calendar
-  
-// }
-
-
-// // Initial Calendar Rendering
-// function calendar_init(year=null){
-//     if (year === null){
-//         year = current_date.getFullYear()
-//     } else{
-//         year = Number(document.getElementById('year-input').value)
-//     }
-//     render_calendar(year)
-// }
-// calendar_init() -> Function Based Renderer call
